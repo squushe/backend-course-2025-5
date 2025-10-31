161e42862adafc1da0f256c6052b08beb4c69ca5
squushe bc2025-5 <maksimrozko49@gmail.com>
2025-10-31 17:33:12 +0200

    Completed second part

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const { program } = require("commander");
 const fs = require("fs");
 const http = require("http");
+const path = require("path");
+const superagent = require("superagent");
 
 program
   .requiredOption("-H, --host <host>")
@@ -16,5 +18,50 @@ if (!fs.existsSync(options.cache)) {
   console.log("Директорію створено");
 }
 
-let server = http.createServer((req, res) => {});
-server.listen(options.port, options.host, () => {});
+let server = http.createServer(async (req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const code = url.pathname.slice(1);
+  const method = req.method;
+  const filePath = path.join(options.cache, `${code}.jpg`);
+
+  switch (method) {
+    case "GET":
+      try {
+        const workData = await fs.promises.readFile(filePath);
+        res.writeHead(200, { "Content-Type": "image/jpeg" });
+        res.end(workData);
+      } catch {
+        res.writeHead(404);
+        res.end("Not Found");
+      }
+      break;
+    case "PUT":
+      const chunks = [];
+      for await (const chunk of req) {
+        chunks.push(chunk);
+      }
+      const data = Buffer.concat(chunks);
+      await fs.promises.writeFile(filePath, data);
+      res.writeHead(201);
+      res.end("Created");
+      break;
+    case "DELETE":
+      try {
+        await fs.promises.unlink(filePath);
+        res.writeHead(200);
+        res.end("Deleted");
+      } catch {
+        res.writeHead(404);
+        res.end("Not Found");
+      }
+      break;
+    default:
+      res.writeHead(405);
+      res.end("Method not allowed");
+      break;
+  }
+});
+
+server.listen(options.port, options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}`);
+});
